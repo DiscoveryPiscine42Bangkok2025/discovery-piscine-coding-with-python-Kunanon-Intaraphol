7154e5e6c2d1736fb760678a5551c7762b2974fb
Kunanon-Intaraphol <86783856+Kunanon-Intaraphol@users.noreply.github.com>
2025-09-15 22:50:56 +0700

    work3

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = 'Kunanon'
+last_name = 'Intharaphol'
+
+print(first_name,last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = 'Kunanon'
+last_name = 'Intharaphol'
+whole_name = first_name +' ' + last_name
+print(whole_name)
