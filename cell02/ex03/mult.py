d33ec27cbf7cea738adb9b2d6008ea4d9ecafe41
Kunanon-Intaraphol <86783856+Kunanon-Intaraphol@users.noreply.github.com>
2025-09-16 01:46:57 +0700

    slab3

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,13 @@
+num1 = int(input('Enter the first number:\n'))
+num2 = int(input('Enter the second number:\n'))
+result= num1*num2
+print(f'{num1} x {num2} = {result}')
+
+if result < 0:
+    print('This number is negative.') 
+elif result == 0:
+    print('This number is both positive and negative.')
+else:
+    print('This number is positive.')
+
+    
