5b3a71c1fd7ea5cdb98ae83a66f43f61faaaf9bf
Kunanon-Intaraphol <86783856+Kunanon-Intaraphol@users.noreply.github.com>
2025-09-17 16:28:27 +0700

    lab5

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,7 +1,7 @@
 num_arr = [2, 8, 9, 48, 8, 22, -12, 2]
 new_arr = []
-for i in range (len(num_arr)):  
-    if num_arr[i] > 5:
-        new_arr.append(num_arr[i]+2)
+for i in num_arr:  
+    if i > 5:
+        new_arr.append(i+2)
 print(num_arr)
 print(new_arr)
